6cb63fb2ca0d2d5edba645ef804cb2e529d73521
Mohit Lahoti <engtechno25@gmail.com>
2023-07-16 22:35:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Book_Name = db.Column(db.String(250), nullable=False, unique=True)
+    Book_Author = db.Column(db.String(250), nullable=False)
+    Rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    ##READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Books).order_by(Books.Book_Name))
+    # Use .scalars() to get the elements rather than entire rows from the database
+    all_books = result.scalars()
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_book = Books(
+            Book_Name=request.form["title"],
+            Book_Author=request.form["author"],
+            Rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add.html")
+
+@app.route("/edit", methods = ["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Books, book_id)
+        book_to_update.Rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Books, book_id)
+    return render_template("edit.html", book=book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
